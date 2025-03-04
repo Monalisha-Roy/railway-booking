f73a1f0b747503242a113e8d57fe0a8aaaf54d02
Monalisha Roy <roymonalisha550@gmail.com>
2025-03-04 16:05:56 +0530

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return(
+        <div className="w-full min-h-screen flex items-center justify-center"> 
+            <h1 className="text-4xl text-green-400">About Page</h1>
+        </div>
+    )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="bg-green h-12">
+            <h1>Navbar</h1>
+        </div>
+    )
+}
